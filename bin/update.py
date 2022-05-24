3efd7db2d3c03ec28e8997ba2a8d7ea0969599d1
Raphaël Vinot <raphael@vinot.info>
2022-05-24 18:58:51 +0200

    chg: Bump deps

--- bin/update.py
+++ bin/update.py
@@ -66,7 +66,8 @@ def main():
 
     print('* Update repository.')
     keep_going(args.yes)
-    run_command('git pull')
+    run_command('git submodule init')
+    run_command('git pull --recurse-submodules')
     new_hash = compute_hash_self()
     if old_hash != new_hash:
         print('Update script changed, please do "poetry run update"')
